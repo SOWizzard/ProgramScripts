4c7511452beb04a3bd215f1b3a9e99424b956307
ruanPetrus <xastroboyx11@gmail.com>
2021-11-12 13:09:19 -0300

    Add alacritty and xprofile

--- Arch/Scripts/alacritty.sh
+++ Arch/Scripts/alacritty.sh
@@ -1,1 +1,5 @@
-sudo pacman -S alacritty
+sudo pacman --noconfirm --needed -S alacritty
+
+projectDir="$(git rev-parse --show-toplevel)"
+mkdir -p "$HOME/.config/alacritty"
+cp "${projectDir}/Arch/CFG/alacritty/default.yml" "${XDG_CONFIG_HOME:-$HOME/.config}/alacritty/alacritty.yml"  
--- Arch/Scripts/windowsManagerInstalation.sh
+++ Arch/Scripts/windowsManagerInstalation.sh
@@ -21,13 +21,21 @@ systemctl enable lightdm
 
 #Logging in user
 su $1
+
+#Xprofile configuration
+projectDir="$(git rev-parse --show-toplevel)"
+cp "${projectDir}/Arch/CFG/xprofile/xprofile" "${HOME}/.xprofile"  
+
 #YAY
-git clone https://aur.archlinux.org/yay-bin.git
-cd yay-bin
+git clone https://aur.archlinux.org/yay-bin.git ~/yay-bin
+cd ~/yay-bin
 makepkg -si
 
 #I3
 bash ./i3.sh
+
+#Alacritty
+bash ./alacritty.sh
 
 #Browser #File manager
 pacman --noconfirm --needed -S firefox
