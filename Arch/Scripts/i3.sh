62480699943806acb8c3bbf9e179a4fe47a83787
ruanPetrus <xastroboyx11@gmail.com>
2021-11-12 10:07:35 -0300

    Script do i3 config do ruan

---
+++ Arch/Scripts/i3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+sudo pacman -Syy
+sudo pacman -S picom
+sudo pacman -S i3-gaps i3status i3lock dmenu
+yay -S autotiling
+
+
+projectDir="$(git rev-parse --show-toplevel)"
+mkdir -p "$HOME/.config/i3"
+mkdir -p "$HOME/.config/picom"
+cp "${projectDir}/Arch/CFG/picom/picom.conf" "${XDG_CONFIG_HOME:-$HOME/.config}/picom/picom.conf"  
+cp "${projectDir}/Arch/CFG/i3/config" "${XDG_CONFIG_HOME:-$HOME/.config}/i3/config"
